9d8f15879e871506f939a4eae84693845b107342
Sachin Bhutani <62913345+Bhutanisachin19@users.noreply.github.com>
2020-08-23 17:13:28 +0530

    Update Keys.js

--- src/Keys.js
+++ src/Keys.js
@@ -1,3 +1,3 @@
-export const API_KEY = "AIzaSyC_pefPlT7nlI0cYjnsboAKR65XWB3jM9E"; //ley from google custom search api
+export const API_KEY = ""; //ley from google custom search api
 
 export default API_KEY;
